2f3de2026f2411b44d1bb18762cfdb689feaa2e4
Abhishek Brahmachary <abhishekb@nihilentanalytics.com>
2019-09-27 14:59:26 +0530

    calender

--- actions/events.py
+++ actions/events.py
@@ -7,8 +7,13 @@ class MyAction(Action):
                       '$select': 'subject,start,end',
                       '$orderby': 'start/dateTime ASC'}
         access_token="e1502f9c-87a4-4b8b-acf0-c41e1658624c"
-        get_events_url="https://graph.microsoft.com/v1.0/me/events/e1502f9c-87a4-4b8b-acf0-c41e1658624c?$select=subject,body,bodyPreview,organizer,attendees,start,end,location"
-        r = requests.get(get_events_url,verify=False)
+        headers= {
+            'Content-Type': 'application/json;charset=UTF-8',
+            'Authorization': "Bearer e1502f9c-87a4-4b8b-acf0-c41e1658624c"
+            }
+        get_events_url= "https://outlook.office.com/api/v2.0/me/calendarview?startdatetime=2015-01-01T00:00:00Z&enddatetime=2015-04-10T00:00:00Z"
+        #get_events_url="https://graph.microsoft.com/v1.0/me/events/e1502f9c-87a4-4b8b-acf0-c41e1658624c?$select=subject,body,bodyPreview,organizer,attendees,start,end,location"
+        r = requests.get(get_events_url,headers=headers,verify=False)
         #r = make_api_call('GET', get_events_url, access_token, parameters = query_parameters)
 
         if (r.status_code == requests.codes.ok):
