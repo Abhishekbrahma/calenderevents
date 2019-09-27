6c0ed9bb9929e829ac67c02a17003852d4da4aa5
Abhishek Brahmachary <abhishekb@nihilentanalytics.com>
2019-09-27 12:38:58 +0530

    first commit

---
+++ actions/events.py
@@ -0,0 +1,10 @@
+import sys
+from st2common.runners.base_action import Action
+
+class MyAction(Action):
+    def run(self, a):
+        for num in range(0,a):
+            for j in range(0,num+1):
+                print("* "),
+            print("\r")
+        return(True,a)
