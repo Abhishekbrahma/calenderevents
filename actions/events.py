5d4f1c72276784dd7502edf7435c899e236a5d8f
Abhishek Brahmachary <abhishekb@nihilentanalytics.com>
2019-09-30 11:42:15 +0530

    new token

--- actions/events.py
+++ actions/events.py
@@ -1,8 +1,7 @@
 import sys
 from st2common.runners.base_action import Action
 import requests
-from urlparse import urlparse
-from urlparse import urlencode
+from urllib.parse import quote, urlencode
 import base64
 import json
 import time
