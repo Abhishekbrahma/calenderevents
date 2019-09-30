c9ee399ed88d3429efcb90773a94b6366013233e
Abhishek Brahmachary <abhishekb@nihilentanalytics.com>
2019-09-30 15:33:22 +0530

    new token

--- actions/Authentication.py
+++ actions/Authentication.py
@@ -7,12 +7,12 @@ from microsoftgraph.client import Client
 class MyAction(Action):
     def run(self ):
 
-    username = 'abhisheknihilent.outlook.com'
-    password = 'M1cr0123'
-    authority = 'OtAGIIR.FzCR6G5N]ra.j@H5OWF7zN.R'
-    resource = ''
+        username = 'abhisheknihilent.outlook.com'
+        password = 'M1cr0123'
+        authority = 'OtAGIIR.FzCR6G5N]ra.j@H5OWF7zN.R'
+        resource = ''
 
-    clientId = 'e1502f9c-87a4-4b8b-acf0-c41e1658624c'
-    client = Client('e1502f9c-87a4-4b8b-acf0-c41e1658624c', 'OtAGIIR.FzCR6G5N]ra.j@H5OWF7zN.R', account_type='by defect common')
-    url = client.authorization_url(redirect_uri, scope, state=None)
-    print(url)
+        clientId = 'e1502f9c-87a4-4b8b-acf0-c41e1658624c'
+        client = Client('e1502f9c-87a4-4b8b-acf0-c41e1658624c', 'OtAGIIR.FzCR6G5N]ra.j@H5OWF7zN.R', account_type='by defect common')
+        url = client.authorization_url(redirect_uri, scope, state=None)
+        print(url)
