d58e26fb59086e76353ce26b01c33f5f0d97eb6a
Abhishek Brahmachary <abhishekb@nihilentanalytics.com>
2019-09-30 15:31:56 +0530

    new token

--- actions/Authentication.py
+++ actions/Authentication.py
@@ -3,7 +3,9 @@ import sys
 import json
 import re
 from adal import AuthenticationContext
+from microsoftgraph.client import Client
 class MyAction(Action):
+    def run(self ):
 
     username = 'abhisheknihilent.outlook.com'
     password = 'M1cr0123'
@@ -11,18 +13,6 @@ class MyAction(Action):
     resource = ''
 
     clientId = 'e1502f9c-87a4-4b8b-acf0-c41e1658624c'
-
-    if username == '' or password == '' or authority == '' or resource == '' or clientId == '':
-        printUsage()
-        sys.exit(-1)
-
-  # Find everything after the last '/' and replace it with 'token'
-    if not authority.endswith('token'):
-        regex = re.compile('^(.*[\/])')
-        match = regex.match(authority)
-        authority = match.group()
-        authority = authority + username.split('@')[1]
-
-    auth_context = AuthenticationContext(authority)
-    token = auth_context.acquire_token_with_username_password(resource, username, password, clientId)
-    print(token["accessToken"])
+    client = Client('e1502f9c-87a4-4b8b-acf0-c41e1658624c', 'OtAGIIR.FzCR6G5N]ra.j@H5OWF7zN.R', account_type='by defect common')
+    url = client.authorization_url(redirect_uri, scope, state=None)
+    print(url)
