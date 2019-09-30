88414ebcc4addf1a949eb1c51950d02f61ff6bff
Abhishek Brahmachary <abhishekb@nihilentanalytics.com>
2019-09-30 14:33:58 +0530

    new token

--- actions/events.py
+++ actions/events.py
@@ -1,16 +1,14 @@
 import sys
 from st2common.runners.base_action import Action
 import requests
-from urlparse import urlparse
-from urlparse import quote, urlencode
+from urllib.parse import quote, urlencode
 import base64
 import json
 import time
 class MyAction(Action):
     def run(self ):
-        print(urllib)
         client_id = 'e1502f9c-87a4-4b8b-acf0-c41e1658624c'
-        client_secret = 'vnr@D6/R[YYk[mEnm5w0eHc0yNAZGNdB'
+        client_secret = 'YOUR APP PASSWORD HERE'
         # Constant strings for OAuth2 flow
         # The OAuth authority
         authority = 'https://login.microsoftonline.com'
@@ -25,22 +23,15 @@ class MyAction(Action):
         scopes = [ 'openid',
                 'User.Read',
             'Mail.Read' ]
-        #access_token="e1502f9c-87a4-4b8b-acf0-c41e1658624c"
+        access_token="e1502f9c-87a4-4b8b-acf0-c41e1658624c"
 
-        get_events_url= "https://outlook.office.com/api/v2.0/me/calendarview?startdatetime=2015-01-01T00:00:00Z&enddatetime=2015-04-10T00:00:00Z"
-        #get_events_url="https://graph.microsoft.com/v1.0/me/events/e1502f9c-87a4-4b8b-acf0-c41e1658624c?$select=subject,body,bodyPreview,organizer,attendees,start,end,location"
-        #r = requests.get(get_events_url,headers=headers,verify=False)
+        #get_events_url= "https://outlook.office.com/api/v2.0/me/calendarview?startdatetime=2015-01-01T00:00:00Z&enddatetime=2015-04-10T00:00:00Z"
+        get_events_url="https://graph.microsoft.com/v1.0/me/events/hxv?OPANLDz3_zf4pLfs_b94??6bN4[C?$select=subject,body,bodyPreview,organizer,attendees,start,end,location"
+        r = requests.get(get_events_url,headers=headers,verify=False)
         #"https://login.microsoftonline.com/{tenant}/oauth2/v2.0/authorize?client_id=e1502f9c-87a4-4b8b-acf0-c41e1658624c&response_type=code&redirect_uri=https://127.0.0.1&response_mode=query&scope=openid%20offline_access%20https%3A%2F%2Fgraph.microsoft.com%2Fuser.read&state=12345"
         #r = make_api_call('GET', get_events_url, access_token, parameters = query_parameters)
-        params={'client_id':client_id,
-                'redirect_url':redirect_uri,
-                'response_type':'code',
-                'scope':' '.join(str(i) for i in scopes)
 
-        }
-        signin_url = authorize_url.format(urlencode(params))
-        print(signin_url)
-        #if (r.status_code == requests.codes.ok):
-            #return r.json()
-        #else:
-            #return "{0}: {1}".format(r.status_code, r.text)
+        if (r.status_code == requests.codes.ok):
+            return r.json()
+        else:
+            return "{0}: {1}".format(r.status_code, r.text)
