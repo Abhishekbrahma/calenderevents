c833c816c3bbd125f14d093281fa7d895f1140be
Abhishek Brahmachary <abhishekb@nihilentanalytics.com>
2019-09-30 14:37:38 +0530

    new token

--- actions/events.py
+++ actions/events.py
@@ -26,7 +26,7 @@ class MyAction(Action):
 
         #get_events_url= "https://outlook.office.com/api/v2.0/me/calendarview?startdatetime=2015-01-01T00:00:00Z&enddatetime=2015-04-10T00:00:00Z"
         get_events_url="https://graph.microsoft.com/v1.0/me/events/hxv?OPANLDz3_zf4pLfs_b94??6bN4[C?$select=subject,body,bodyPreview,organizer,attendees,start,end,location"
-        r = requests.get(get_events_url,headers=headers,verify=False)
+        r = requests.get(get_events_url,verify=False)
         #"https://login.microsoftonline.com/{tenant}/oauth2/v2.0/authorize?client_id=e1502f9c-87a4-4b8b-acf0-c41e1658624c&response_type=code&redirect_uri=https://127.0.0.1&response_mode=query&scope=openid%20offline_access%20https%3A%2F%2Fgraph.microsoft.com%2Fuser.read&state=12345"
         #r = make_api_call('GET', get_events_url, access_token, parameters = query_parameters)
 
